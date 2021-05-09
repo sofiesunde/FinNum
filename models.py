1b9784415ba39e82dcff8c8ae69dd22ffbe96a63
Sofie Sunde <evasofies@gmail.com>
2021-05-09 14:07:53 +0200

    Update models.py

--- models.py
+++ models.py
@@ -5,21 +5,3 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import VotingClassifier
 
-# Support Vector Machine Classifier
-class SupportVectorMachineClassifier():
-    def __init__(self):
-        self.svmClassifier = make_pipeline(StandardScaler(), SVC())
-
-# Random Forest Classifier
-class RandomForestClassifier():
-    def __init__(self):
-        self.rfClassifier = RandomForestClassifier()
-
-# Ensamble Classifier of SVM and Random Forest Classifiers
-class EnsembleClassifier():
-    def __init__(self):
-        svm = make_pipeline(StandardScaler(), SVC())
-        rf = RandomForestClassifier()
-
-        self.votingClassifier = VotingClassifier(
-            classifiers=[('Support Vector Machine', svm), ('Random Forest', rf)], voting='hard')
